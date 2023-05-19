b300653d78d2dc05624ea89a607325fe68b82f19
mohitttb <mohit.bhagwani@fiftyfivetech.io>
2023-05-19 15:16:53 +0530

    assignment 2 completed
    assignment 2 completed
    
    assignment 2 completed

---
+++ src/main/java/fiftyfive/administration/usermanagement/entity/User.java
@@ -0,0 +1,34 @@
+package fiftyfive.administration.usermanagement.entity;
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user", schema = "public")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "role", nullable = false)
+    private String role;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}

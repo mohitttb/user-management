07534b3677ff287e4daedb3cd4854b3bd18eb809
mohitttb <mohit.bhagwani@fiftyfivetech.io>
2023-05-19 10:44:19 +0530

    removed unwanted comments

--- src/main/java/fiftyfive/administration/usermanagement/dto/CreateUserRequest.java
+++ src/main/java/fiftyfive/administration/usermanagement/dto/CreateUserRequest.java
@@ -1,41 +1,29 @@
 package fiftyfive.administration.usermanagement.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 
-
 @Data
 public class CreateUserRequest {
-    @JsonProperty(value = "username",required = true)
-//    @NotEmpty(message = "Username is required")
+    @JsonProperty(value = "username", required = true)
     @NotNull(message = "Username is required")
-//    @NotBlank
     private String username;
 
-    @JsonProperty(value = "password",required = true)
-//    @NotEmpty(message = "Password is required")
+    @JsonProperty(value = "password", required = true)
     @NotNull(message = "Password is required")
-//    @NotBlank
     private String password;
 
-    @JsonProperty(value = "first_name",required = true)
-//    @NotEmpty(message = "First Name is required")
+    @JsonProperty(value = "first_name", required = true)
     @NotNull(message = "First Name is required")
-//    @NotBlank
     private String firstName;
 
-    @JsonProperty(value = "last_name",required = true)
-//    @NotEmpty(message = "Last Name is required")
+    @JsonProperty(value = "last_name", required = true)
     @NotNull(message = "Last Name is required")
-//    @NotBlank
     private String lastName;
 
-    @JsonProperty(value = "role",required = true)
-//    @NotEmpty(message = "Role is required")
+    @JsonProperty(value = "role", required = true)
     @NotNull(message = "Role is required")
-//    @NotBlank
     private String role;
 }
